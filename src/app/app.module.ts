b279a7a32b5427647a592ae4817c21665740889f
Paolo <paolo.licup@catalisgov.com>
2023-09-26 00:53:45 +0800

    routing setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
     declarations: [
         AppComponent
     ],
     imports: [
+        AppRoutingModule,
         BrowserModule
     ],
     providers: [],
