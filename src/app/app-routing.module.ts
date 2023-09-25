ae175220257d409d12fe3b87dd52dcd8f65514cf
Paolo <paolo.licup@catalisgov.com>
2023-09-26 05:00:42 +0800

    initial components with routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
+import { RecipeComponent } from "./recipe/recipe.component";
+import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
 
 const appRoutes: Routes = [
-    { path: '', redirectTo: '/recipes', pathMatch: 'full' }
+    { path: '', redirectTo: '/recipes', pathMatch: 'full' },
+    { path: 'recipes', component: RecipeComponent },
+    { path: 'shopping-list', component: ShoppingListComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { RecipeComponent } from './recipe/recipe.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 
 @NgModule({
     declarations: [
-        AppComponent
+        AppComponent,
+        RecipeComponent,
+        ShoppingListComponent
     ],
     imports: [
         AppRoutingModule,
